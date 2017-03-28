c9c2537a46547a508a5ba13b2ea67841386b0d9f
Tu Nguyen <nvminhtu@gmail.com>
2017-03-28 15:34:03 +0700

    update news custom post type

--- includes/custom-post-types.php
+++ includes/custom-post-types.php
@@ -114,6 +114,48 @@ function prefix_register_all() {
 
 	register_taxonomy( 'qacat', array( 'qa' ), $args );
 
+	/* 03 -- news ------- */
+	register_post_type(
+		'news',
+		array(
+			'labels'        => array(
+				'name'               => __('お知らせ', 'custom_text_posttype'),
+				'singular_name'      => __('お知らせ', 'custom_text_posttype'),
+				'menu_name'          => __('お知らせ', 'custom_text_posttype'),
+				'name_admin_bar'     => __('お知らせ', 'custom_text_posttype'),
+				'all_items'          => __('All Items', 'custom_text_posttype'),
+				'add_new'            => _x('Add New', 'news', 'custom_text_posttype'),
+				'add_new_item'       => __('Add New Item', 'custom_text_posttype'),
+				'edit_item'          => __('Edit Item', 'custom_text_posttype'),
+				'new_item'           => __('New Item', 'custom_text_posttype'),
+				'view_item'          => __('View Item', 'custom_text_posttype'),
+				'search_items'       => __('Search Items', 'custom_text_posttype'),
+				'not_found'          => __('No items found.', 'custom_text_posttype'),
+				'not_found_in_trash' => __('No items found in Trash.', 'custom_text_posttype'),
+				'parent_item_colon'  => __('Parent Items:', 'custom_text_posttype'),
+			),
+			'public'        => true,
+			'menu_position' => 3,
+			'supports'      => array(
+				'title',
+				'editor',
+				'thumbnail',
+				'excerpt',
+				'author',
+				'revisions',
+				'custom-fields'
+			),
+			'taxonomies'    => array(
+				'post_tag'
+			),
+			'has_archive'   => true,
+			'menu_icon'   => 'dashicons-welcome-write-blog',
+			'rewrite'       => array(
+				'slug' => 'news',
+			),
+		)
+	);
+
 }
 
 add_action('init', 'prefix_register_all', 0);
