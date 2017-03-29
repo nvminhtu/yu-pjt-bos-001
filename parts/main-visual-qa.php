e292a25c1bc065ae980559629fe7032fdf2e4a1b
Tu Nguyen <nvminhtu@gmail.com>
2017-03-29 15:21:10 +0700

    load private main visual - only for qa

--- parts/main-visual-qa.php
+++ parts/main-visual-qa.php
@@ -7,13 +7,13 @@
  <div class="inner">
    <div class="main_pic">
      <?php
-        $page_main_visual_sp = get_field('page_main_visual_sp');
-        $page_main_visual_tablet = get_field('page_main_visual_tablet');
-        $page_main_visual_pc = get_field('page_main_visual_pc');
+        $qa_main_visual_sp = get_field('qa_main_visual_sp','option');
+        $qa_main_visual_tablet = get_field('qa_main_visual_tablet','option');
+        $qa_main_visual_pc = get_field('qa_main_visual_pc','option');
      ?>
-     <img class="sp" src="<?php echo $page_main_visual_sp; ?>" />
-     <img class="tablet" src="<?php echo $page_main_visual_tablet; ?>" />
-     <img class="pc" src="<?php echo $page_main_visual_pc; ?>" />
+     <img class="sp" src="<?php echo $qa_main_visual_sp; ?>" />
+     <img class="tablet" src="<?php echo $qa_main_visual_tablet; ?>" />
+     <img class="pc" src="<?php echo $qa_main_visual_pc; ?>" />
    </div>
    <h2><span><?php the_title(); ?></span></h2>
  </div>
