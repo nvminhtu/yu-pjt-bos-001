eec83dc70bd26c39880d58ca2939b29bfc2c5fe7
Tu Nguyen <nvminhtu@gmail.com>
2017-03-30 10:21:33 +0700

    update metabox for Studio (CMB2 map)

--- includes/metabox.php
+++ includes/metabox.php
@@ -1,74 +1,34 @@
-<?php 
-add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
+<?php
+add_action( 'cmb2_admin_init', 'cmb2_other_posts_metaboxes' );
 /**
  * Define the metabox and field configurations.
  */
-function cmb2_sample_metaboxes() {
+function cmb2_other_posts_metaboxes() {
 
     // Start with an underscore to hide fields from custom fields list
-    $prefix = '_traijing_';
+    $prefix = '_bosty_';
 
     /**
      * Initiate the metabox
      */
-    $cmb = new_cmb2_box( array(
-        'id'            => 'traijing_metabox',
-        'title'         => __( 'Traijing Related Posts', 'cmb2' ),
-        'object_types'  => array( 'post', ), // Post type
-        'context'       => 'normal',
-        'priority'      => 'high',
-        'show_names'    => true, // Show field names on the left
-    ) );
+     $cmb = new_cmb2_box( array(
+         'id'            => 'bosty_metabox',
+         'title'         => __( 'Studio Map', 'cmb2' ),
+         'object_types'  => array( 'studio', ), // Post type
+         'context'       => 'normal',
+         'priority'      => 'high',
+         'show_names'    => true, // Show field names on the left
+     ) );
 
-  
-    $cmb->add_field( array(
-        'name'       => __( 'Select Related Posts', 'cmb2' ),
-        'desc'       => __( 'check a post or multi posts', 'cmb2' ),
-        'id'         => $prefix . 'post_multicheckbox',
-        'type'       => 'multicheck',
-        'options_cb' => 'cmb2_get_your_post_type_post_options',
-    ) );
-    // Add other metaboxes as needed
+     $cmb->add_field( array(
+         'name'       => __( 'Select Map Position', 'cmb2' ),
+         'desc'       => __( 'Input your address / use maker to check your location', 'cmb2' ),
+         'id'         => $prefix . 'google_map',
+         'type'       => 'pw_map'
+     ) );    // Add other metaboxes as needed
 
 }
 
-/**
- * Gets a number of posts and displays them as options
- * @param  array $query_args Optional. Overrides defaults.
- * @return array             An array of options that matches the CMB2 options array
- */
-function cmb2_get_post_options( $query_args ) {
-    global $post, $typenow;
-
-    $current_user = wp_get_current_user();
-    $author_name = $current_user->user_login;
-    $author_id = $post->post_author;
-    
-    $args = array(
-        'post_type'   => 'post',
-        'numberposts' => 10,
-        'author' => $author_id,
-        'post_status' => 'publish,future',
-        'exclude'     => $post->ID
-    );
-
-    $posts = get_posts( $args );
-
-    $post_options = array();
-    if ( $posts ) {
-        foreach ( $posts as $post ) {
-          $post_options[ $post->ID ] = $post->post_title;
-        }
-    }
-
-    return $post_options;
-}
-
-/**
- * Gets 5 posts for your_post_type and displays them as options
- * @return array An array of options that matches the CMB2 options array
- */
 function cmb2_get_your_post_type_post_options() {
     return cmb2_get_post_options( );
 }
-      
