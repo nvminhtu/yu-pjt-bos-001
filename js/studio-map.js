0fa60a2b0378d9fc6e969099bd722077af83f110
Tu Nguyen <nvminhtu@gmail.com>
2017-03-30 17:12:50 +0700

    load studio data in to page.

--- js/studio-map.js
+++ js/studio-map.js
@@ -1,18 +1,24 @@
 $(function() {
-  var map;
-  var viewMapLarger = 'https://www.google.com/maps/place/' + '49.46800006494457' + ',' + '17.11514008755796';
+  var map, latitude, longitude;
+  
+  var latitude = $('.maparea').data('lat'),
+      longitude = $('.maparea').data('long');
+
+  var viewMapLarger = 'https://www.google.com/maps/place/' + latitude + ',' + longitude;
   var html = '<div class="place-name">ホテル＆レジデンス六本木</div>'+
                 '<div class="place-address">Nhật Bản, 〒106-0031 Tōkyō-to, Minato-ku, Nishiazabu, 1 Chome−11−6, ホテル＆レジデンス六本木</div>'
                  + '<div class="place-view"><a target="_blank" href="'+ viewMapLarger +'">Google Mapで見る</a></div>';
+
+  // init map
   map = new GMaps({
     el: '#gmap',
-    lat: 35.660356,
-    lng: 139.72454600000003,
+    lat: latitude,
+    lng: longitude,
     zoom: 17
   });
   map.addMarker({
-      lat: 35.660356,
-      lng: 139.72454600000003,
+      lat: latitude,
+      lng: longitude,
       infoWindow: {
         content: html
       }
