1029e690b40c934657183f9cc37c33bd8c104293
DESKTOP-IVJRVD7\TU <nvminhtu@gmail.com>
2017-04-01 09:06:00 +0700

    fix instagram loading on homepage

--- parts/instagram.php
+++ parts/instagram.php
@@ -4,21 +4,7 @@
   */
 ?>
 <?php 
-	$insta_username = '';
+	$insta_username = 'bosty_official';
 	echo do_shortcode('[ap_instagram_slider set_username="'.$insta_username.'"]'); 
 ?>
-<div id="instagram" class="instagram clearfix">
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_14.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_15.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_16.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_17.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_14.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_15.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_16.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_17.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_14.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_15.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_16.jpg" alt="Gallery ギャラリー" /></div>
-	<div class="slide"><img src="<?php bloginfo('template_url'); ?>/images/index_img_17.jpg" alt="Gallery ギャラリー" /></div>
-</div>
 <p class="inner index_img_10 text-center"><a href="https://www.instagram.com/bosty_official/?hl=ja" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/index_img_10.png" alt="BOSTY Instagram を見る" /></a></p>
