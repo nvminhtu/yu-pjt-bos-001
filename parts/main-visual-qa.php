91a6aa24e8b55e2084754aa7e9de36ed09e3c96f
DESKTOP-IVJRVD7\TU <nvminhtu@gmail.com>
2017-04-05 01:41:24 +0700

    fix main visual display

--- parts/main-visual-qa.php
+++ parts/main-visual-qa.php
@@ -3,36 +3,34 @@
  ** Main Visual in qa pages
  */
 ?>
+<?php
+  $page_main_visual_sp = get_field('page_main_visual_sp');
+  $page_main_visual_tablet = get_field('page_main_visual_tablet');
+  $page_main_visual_pc = get_field('page_main_visual_pc');
+?>
 <div class="com_mainvisual">
- <div class="inner">
-   <div class="main_pic">
-     <?php
-        $qa_main_visual_sp = get_field('qa_main_visual_sp','option');
-        $qa_main_visual_tablet = get_field('qa_main_visual_tablet','option');
-        $qa_main_visual_pc = get_field('qa_main_visual_pc','option');
-     ?>
-     <img class="sp" src="<?php echo $qa_main_visual_sp; ?>" />
-     <img class="tablet" src="<?php echo $qa_main_visual_tablet; ?>" />
-     <img class="pc" src="<?php echo $qa_main_visual_pc; ?>" />
-   </div>
-   <h2><span><?php
-    if(is_single())
-    {
-      the_title();
-    } 
-    elseif(is_archive())
-    {
-      if(is_post_type_archive('qa'))
-      {
-        echo "QA";
-      }
-      elseif(is_tax('qacat'))
-      {
-        single_term_title();
-      }
+  <div class="inner">
+    <h2 id="title-background" data-pc="<?php echo $page_main_visual_pc; ?>" data-tb="<?php echo $page_main_visual_tablet; ?>" data-sp="<?php echo $page_main_visual_sp; ?>">
+      <span>
+        <?php
+          if(is_single())
+          {
+            the_title();
+          } 
+          elseif(is_archive())
+          {
+            if(is_post_type_archive('qa'))
+            {
+              echo "QA";
+            }
+            elseif(is_tax('qacat'))
+            {
+              single_term_title();
+            }
 
-    }
-    ?></span></h2>
-   }
- </div>
+          }
+        ?>
+      </span>
+    </h2>
+  </div>
 </div>
