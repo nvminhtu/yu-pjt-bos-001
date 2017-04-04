4aade4b47ae950295d7857810afb4538570efd83
DESKTOP-IVJRVD7\TU <nvminhtu@gmail.com>
2017-04-05 02:27:47 +0700

    add links to page in footer

--- footer.php
+++ footer.php
@@ -3,21 +3,21 @@
   <div class="inner clearfix">
     <div class="f_column_01">
       <ul>
-        <li><a href="./">HOME</a></li>
+        <li><a href="<?php bloginfo('siteurl'); ?>">HOME</a></li>
         <li>&nbsp;</li>
-        <li><a href="studio.html">スタジオ一覧</a></li>
-        <li><a href="price.html">プログラム・料金</a></li>
-        <li><a href="feature.html">BOSTYの特徴</a></li>
-        <li><a href="qa.html">よくあるご質問</a></li>
+        <li><a href="<?php bloginfo('siteurl'); ?>/studio/">スタジオ一覧</a></li>
+        <li><a href="<?php bloginfo('siteurl'); ?>/price/">プログラム・料金</a></li>
+        <li><a href="<?php bloginfo('siteurl'); ?>/feature/">BOSTYの特徴</a></li>
+        <li><a href="<?php bloginfo('siteurl'); ?>/qa/">よくあるご質問</a></li>
       </ul>
     </div>
     <div class="f_column_02">
       <ul>
         <li>&nbsp;</li>
         <li>&nbsp;</li>
-        <li><a href="company.html">会社概要</a></li>
-        <li><a href="#">お知らせ</a></li>
-        <li><a href="contact.html">問い合わせ</a></li>
+        <li><a href="<?php bloginfo('siteurl'); ?>/company/">会社概要</a></li>
+        <li><a href="<?php bloginfo('siteurl'); ?>/news/">お知らせ</a></li>
+        <li><a href="<?php bloginfo('siteurl'); ?>/contact/">問い合わせ</a></li>
       </ul>
     </div>
     <div class="f_column_03">
@@ -30,7 +30,7 @@
         <dt><img src="<?php bloginfo('template_url'); ?>/images/f_ico_02.png"></dt>
         <dd><a href="tel:0362058288">03-6205-8288</a></dd>
       </dl>
-      <p class="btn_02 index_img_13"><a href="contact.html"><img src="<?php bloginfo('template_url'); ?>/images/index_img_13.png">問い合わせはこちら</a></p><br>
+      <p class="btn_02 index_img_13"><a href="<?php bloginfo('siteurl'); ?>/contact/"><img src="<?php bloginfo('template_url'); ?>/images/index_img_13.png">問い合わせはこちら</a></p><br>
     </div>
     <div class="f_column_04">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2725.6407232018696!2d139.74793133241303!3d35.668350020053715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b924f7c38d5:0xa54fe3fcd884da49!2z5pel5pysLCDjgJIxMDUtMDAwMSDmnbHkuqzpg73muK_ljLromY7jg47ploDvvJHkuIHnm67vvJHvvJbiiJLvvJHvvJEg5qGc5bed44Ki44ON44OD44Kv44K5!5e0!3m2!1sja!2s!4v1487078167072" width="100%" height="204" frameborder="0" style="border:0" allowfullscreen></iframe>
