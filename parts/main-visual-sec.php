d991c527f3011a6b82d14d219159dfefe34a24ce
DESKTOP-IVJRVD7\TU <nvminhtu@gmail.com>
2017-04-05 02:56:41 +0700

    add visual media for all page

--- parts/main-visual-sec.php
+++ parts/main-visual-sec.php
@@ -8,6 +8,25 @@
   $page_main_visual_tablet = get_field('page_main_visual_tablet');
   $page_main_visual_pc = get_field('page_main_visual_pc');
 ?>
+<style>
+	/* pc */
+	.com_mainvisual h2 {
+		background: url('<?php echo $page_main_visual_pc; ?>') bottom center no-repeat!important;	
+	} 
+	/* tablet*/
+	@media screen and (max-width: 1200px) {
+		.com_mainvisual h2 {
+			background: url('<?php echo $page_main_visual_tablet; ?>') bottom center no-repeat!important;
+		} 
+	}
+	/* mobile */
+	@media screen and (max-width: 768px) {
+		.com_mainvisual h2 {
+			background: url('<?php echo $page_main_visual_sp; ?>') bottom center no-repeat!important;	
+		} 
+	}
+	
+</style>
 <div class="com_mainvisual">
   <div class="inner">
     <h2 id="title-background" data-pc="<?php echo $page_main_visual_pc; ?>" data-tb="<?php echo $page_main_visual_tablet; ?>" data-sp="<?php echo $page_main_visual_sp; ?>"><span><?php the_title(); ?></span></h2>
