f98707f6837f13beaa088ff36adc20840f97b58d
Tu Nguyen <nvminhtu@gmail.com>
2017-05-06 17:33:08 +0700

    fix description for price

--- page-price.php
+++ page-price.php
@@ -13,14 +13,18 @@
 
       $tags = array("<p>", "</p>", "<font>", "</font>");
       // Content break
-      $experienced_learning = get_field('experienced_learning');
-      $eating_learning = get_field('eating_learning');
       $special_learning = get_field('special_learning');
       // -----------------------------------------------------------------------------
     ?>
     <section id="section_01" class="section_01">
       <div class="inner">
-        <?php echo $experienced_learning; ?>
+        <?php
+          $experienced_learning = get_field('experienced_learning');
+          $experienced_learning_description = get_field('experienced_learning_description');
+          $experienced_learning_content = get_field('experienced_learning_content');
+        ?>
+        <h2 class="title_01"><?php echo $experienced_learning; ?><span><?php echo $experienced_learning_description; ?></span></h2>
+        <p class="text_01"><?php echo strip_tags($experienced_learning_content,'<br>,<br />'); ?></p>
         <?php
             $i= 1;
             $class_row = "";
@@ -38,14 +42,37 @@
             $course_title = get_sub_field('course_title');
             $course_description = get_sub_field('course_description');
             $course_content = get_sub_field('course_content');
+            $course_content_table = get_sub_field('course_content_table');
             $course_picture_id = get_sub_field('course_picture');
             $course_picture_src = wp_get_attachment_image_src( $course_picture_id, 'img_price_col3');
           ?>
           <li>
             <h3 class="clearfix heightLine-a1 h_resauto"><img src="<?php bloginfo('template_url'); ?>/images/price/price_img_01.png" alt="コース1" /><span>コース</span><span class="number"><?php echo $i; ?></span><?php echo $course_title; ?></h3>
             <img src="<?php echo $course_picture_src[0]; ?>" alt="<?php echo $course_title; ?>" />
-            <?php echo $course_description; ?>
-            <?php echo $course_content; ?>
+            <p class="lineh_01 heightLine-a9 h_resauto"><?php echo strip_tags($course_description,'<br>,<br />'); ?></p>
+            <?php
+              $j = 0;
+              while( have_rows('course_content_table') ): the_row();
+                $course_table_membership = get_sub_field('course_table_membership');
+                $course_table_membership_description = get_sub_field('course_table_membership_description');
+                $course_table_price = get_sub_field('course_table_price');
+                $course_table_price_description = get_sub_field('course_table_price_description');
+                $course_table_bottom = get_sub_field('course_table_bottom');
+            ?>
+            <table class="table_01 heightLine-a3 h_resauto">
+              <tbody>
+                <tr>
+                  <th>入会金</th>
+                  <td><?php echo $course_table_membership; ?><?php if($course_table_membership_description!='') { ?><span><?php echo $course_table_membership_description; ?></span><?php } ?></td>
+                </tr>
+                <tr>
+                  <th>コース料金</th>
+                  <td><?php echo $course_table_price; ?><span><?php echo $course_table_price_description; ?></span></td>
+                </tr>
+              </tbody>
+            </table>
+            <p class="note"><?php echo strip_tags($course_table_bottom); ?></p>
+            <?php $j++; endwhile; ?>
           </li>
           <?php $i++; endwhile; ?>
           </ul>
@@ -57,8 +84,13 @@
 
     <section id="section_02" class="section_02">
       <div class="inner">
-        <?php echo $eating_learning; ?>
-
+        <?php
+          $eating_learning = get_field('eating_learning');
+          $eating_learning_description = get_field('eating_learning_description');
+          $eating_learning_content = get_field('eating_learning_content');
+        ?>
+        <h2 class="title_01"><?php echo $eating_learning; ?><span><?php echo $eating_learning_description; ?></span></h2>
+        <p class="text_01"><?php echo strip_tags($eating_learning_content,'<br>,<br />'); ?></p>
         <?php
           $i = 1;
           if( have_rows('course_information_section2') ):
@@ -68,7 +100,6 @@
           <?php while( have_rows('course_information_section2') ): the_row();
             $course_title = get_sub_field('course_title');
             $course_description = get_sub_field('course_description');
-            $course_content = get_sub_field('course_content');
             $course_picture_id = get_sub_field('course_picture');
             $course_picture_src = wp_get_attachment_image_src( $course_picture_id, 'img_price_col2_sec2');
           ?>
@@ -76,8 +107,28 @@
               <div class="list_02_inner">
                 <h3><img src="<?php bloginfo('template_url'); ?>/images/price/price_img_05.png" alt="コース<?php echo $i; ?>" /><span>コース</span><span class="number"><?php echo $i; ?></span><?php echo $course_title; ?></h3>
                 <img src="<?php echo $course_picture_src[0]; ?>" alt="<?php echo $course_title; ?>" />
-                <?php echo $course_description; ?>
-                <?php echo $course_content; ?>
+                <p class="lineh_02 heightLine-a4 h_resauto02"><?php echo strip_tags($course_description,'<br>,<br />'); ?></p>
+                <?php
+                  $j = 0;
+                  while( have_rows('course_content_table') ): the_row();
+                    $course_table_membership = get_sub_field('course_table_membership');
+                    $course_table_membership_description = get_sub_field('course_table_membership_description');
+                    $course_table_price = get_sub_field('course_table_price');
+                    $course_table_price_description = get_sub_field('course_table_price_description');
+                ?>
+                <table class="table_02 heightLine-a5 h_resauto02">
+                  <tbody>
+                    <tr>
+                      <th>入会金</th>
+                      <td><?php echo $course_table_membership; ?><?php if($course_table_membership_description!='') { ?><span><?php echo $course_table_membership_description; ?></span><?php } ?></td>
+                    </tr>
+                    <tr>
+                      <th>コース料金</th>
+                      <td><?php echo $course_table_price; ?><span><?php echo $course_table_price_description; ?></span></td>
+                    </tr>
+                  </tbody>
+                </table>
+                <?php $j++; endwhile; ?>
               </div>
             </li>
           <?php $i++; endwhile; ?>
@@ -90,7 +141,13 @@
     </section>
     <section id="section_03" class="section_03">
       <div class="inner">
-        <?php echo $special_learning; ?>
+        <?php
+          $special_learning = get_field('special_learning');
+          $special_learning_description = get_field('special_learning_description');
+          $special_learning_content = get_field('special_learning_content');
+        ?>
+        <h2 class="title_01"><?php echo $special_learning; ?><span><?php echo $special_learning_description; ?></span></h2>
+        <p class="text_01"><?php echo strip_tags($special_learning_content,'<br>,<br />'); ?></p>
       </div>
       <?php
           $i = 1;
@@ -101,15 +158,36 @@
           <?php while( have_rows('course_information_sec3') ): the_row();
             $course_title = get_sub_field('course_title');
             $course_description = get_sub_field('course_description');
-            $course_content = get_sub_field('course_content');
             $course_picture_id = get_sub_field('course_picture');
             $course_picture_src = wp_get_attachment_image_src( $course_picture_id, 'img_price_col2');
           ?>
           <li>
             <h3 class="clearfix heightLine-a6 h_resauto"><img src="<?php bloginfo('template_url'); ?>/images/price/price_img_01.png" alt="コース<?php echo $i; ?>" /><span>コース</span><span class="number"><?php echo $i; ?></span></span><?php echo $course_title; ?></h3>
             <img src="<?php echo $course_picture_src[0]; ?>" alt="<?php echo $course_title; ?>" />
-            <?php echo $course_description; ?>
-            <?php echo $course_content; ?>
+            <p class="lineh_03 heightLine-a7 h_resauto"><?php echo strip_tags($course_description,'<br>,<br />'); ?></p>
+            <?php
+              $j = 0;
+              while( have_rows('course_content_table') ): the_row();
+                $course_table_membership = get_sub_field('course_table_membership');
+                $course_table_membership_description = get_sub_field('course_table_membership_description');
+                $course_table_price = get_sub_field('course_table_price');
+                $course_table_price_description = get_sub_field('course_table_price_description');
+                $course_table_bottom = get_sub_field('course_table_bottom');
+            ?>
+            <table class="table_01 heightLine-a8 h_resauto">
+              <tbody>
+                <tr>
+                  <th>入会金</th>
+                  <td><?php echo $course_table_membership; ?><?php if($course_table_membership_description!='') { ?><span><?php echo $course_table_membership_description; ?></span><?php } ?></td>
+                </tr>
+                <tr>
+                  <th>コース料金</th>
+                  <td><?php echo $course_table_price; ?><span><?php echo $course_table_price_description; ?></span></td>
+                </tr>
+              </tbody>
+            </table>
+            <p class="bottom"><?php echo strip_tags($course_table_bottom); ?></p>
+            <?php $j++; endwhile; ?>
           </li>
 
            <?php $i++; endwhile; ?>
