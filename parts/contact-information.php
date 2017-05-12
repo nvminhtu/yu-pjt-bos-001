4bb4236777084b5c697eebe70dd2d763d2069914
Tu Nguyen <nvminhtu@gmail.com>
2017-05-12 12:47:05 +0700

    fix contact information button - model and tel..

--- parts/contact-information.php
+++ parts/contact-information.php
@@ -12,10 +12,13 @@
     <p class="btn_03"><a href="<?php echo $contact_link; ?>"><span><?php echo $contact_text; ?></span></a></p>
   </div>
 </section>
-<section id="section_08" class="footer_section_03">
-  <div class="inner"> <img src="<?php bloginfo('template_url'); ?>/images/feature/feature_img_18.png" alt="お電話からでも、お気軽にご相談ください。" class="pc" /> <img src="<?php bloginfo('template_url'); ?>/images/feature/feature_img_18_tb.png" alt="お電話からでも、お気軽にご相談ください。" class="tablet" /> <img src="<?php bloginfo('template_url'); ?>/images/feature/feature_img_18_tb.png" alt="お電話からでも、お気軽にご相談ください。" class="sp" />
+<section id="section_08" class="footer_section_03 info-bottom">
+  <div class="inner">
+    <img src="<?php bloginfo('template_url'); ?>/images/feature/feature_img_18.png" alt="お電話からでも、お気軽にご相談ください。" class="pc" />
+    <img src="<?php bloginfo('template_url'); ?>/images/feature/feature_img_18_tb.png" alt="お電話からでも、お気軽にご相談ください。" class="tablet" />
+    <img src="<?php bloginfo('template_url'); ?>/images/feature/feature_img_18_tb.png" alt="お電話からでも、お気軽にご相談ください。" class="sp" />
     <div class="footer_tel"> <?php echo $contact_description; ?>
-      <a href="tel:<?php echo $contact_phone; ?>"><?php echo $contact_phone; ?></a>
+      <a class="tel-number" href="tel:<?php echo $contact_phone; ?>"><?php echo $contact_phone; ?></a>
       <?php if($contact_note) {
           echo $contact_note;
       } else { ?>
